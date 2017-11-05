c6cecfed4989e1a4bf10a778fcbce2074d10082a
Steffan P <stp1g15@soton.ac.uk>
2017-11-05 20:45:51 +0000

    Neatened up the dictionary a bit, also remove the dummy entry.

--- dictionary.py
+++ dictionary.py
@@ -11,16 +11,15 @@ class Dictionary:
 
     def lookup_json(self, word):
         entry = {'de': word, 'en': [], 'root': '', 'category': '', 'grammar': []}
-        initial_entry = {'de': word, 'en': [], 'root': '', 'category': '', 'grammar': []}
         in_dict, entry = self._lemmatron(entry)
         if in_dict:
             en_found, entry = self._lookup_online(entry)
             if en_found:
                 return json.dumps(entry, ensure_ascii=False), 200
             else:
-                return json.dumps(initial_entry, ensure_ascii=False), 404
+                return json.dumps(entry, ensure_ascii=False), 404
         else:
-            return json.dumps(initial_entry, ensure_ascii=False), 404
+            return json.dumps(entry, ensure_ascii=False), 404
 
     def _lemmatron(self, entry):
         word = entry['de'].lower()
@@ -29,9 +28,10 @@ class Dictionary:
             r_json = r.json()['results']
             entry['root'] = r_json[0]['lexicalEntries'][0]['inflectionOf'][0]['text']
             entry['category'] = r_json[0]['lexicalEntries'][0]['lexicalCategory'].lower()
-            entry['grammar'] = sort_grammar(r_json[0]['lexicalEntries'][0]['grammaticalFeatures'])
+            entry['grammar'] = self.sort_grammar(r_json[0]['lexicalEntries'][0]['grammaticalFeatures'])
             return True, entry
         else:
+            entry['category'] = "unknown"
             return False, entry
 
     def _lookup_online(self, entry):
@@ -42,7 +42,7 @@ class Dictionary:
             r_json = r.json()['results']
             for lexEntry in r_json[0]['lexicalEntries']:
                 if lexEntry['lexicalCategory'].lower() == cat:
-                    entry['en'] = sort_english(lexEntry['entries'])
+                    entry['en'] = self.sort_english(lexEntry['entries'])
                     if len(entry['en']) != 0:
                         return True, entry
             return False, entry
@@ -50,88 +50,91 @@ class Dictionary:
             return False, entry
 
     def lookup_html(self, word):
-        # use the already written json code, then pass it into a DictEntry object.
-        # but we're not using that right now
-
-        # entry_json, status_code = self.lookup_json(word)
-        status_code = 200
-        entry_json = "{\"category\": \"noun\", \"grammar\": [{\"Gender\": \"Feminine\", \"Case\": \"Dative\", \"Number\": \"Singular\"}, {\"Gender\": \"Feminine\", \"Case\": \"Genitive\", \"Number\": \"Singular\"}, {\"Gender\": \"Feminine\", \"Case\": \"Accusative\", \"Number\": \"Singular\"}], \"de\": \"Sahne\", \"en\": [\"cream\"], \"root\": \"Sahne\"}"
+        entry_json, status_code = self.lookup_json(word)
         return DictEntry(json.loads(entry_json), status_code)
+
+    @staticmethod
+    def sort_grammar(gram_fe):
+        counter = {}
+        # count the occurrences of each grammatical type
+        for feature in gram_fe:
+            g_type = feature['type']
+            if g_type in counter.keys():
+                counter[g_type] += 1
+            else:
+                counter[g_type] = 1
+        # determine the maximum
+        maximum = max(counter, key=(lambda key: counter[key]))
+        # create the empty dictionaries
+        sorted = [{} for _ in range(counter[maximum])]
+
+        # create another dictionary to keep track of how many times a type has occurred while sorting.
+        occurred = {}
+        for key in counter.keys():
+            occurred[key] = 0
+
+        # sort the features
+        for i in range(len(gram_fe)):
+            g_type = gram_fe[i]['type']
+            text = gram_fe[i]['text']
+            o = occurred[g_type]
+            sorted[o][g_type] = text
+            if o == counter[g_type] - 1:
+                for j in range(o, len(sorted)):
+                    sorted[j][g_type] = text
+            occurred[g_type] += 1
+
+        return sorted
+
+    @staticmethod
+    def sort_english(entries):
+        en = []
+        for entry in entries:
+            if 'senses' in entry.keys():
+                senses = entry['senses']
+                for sense in senses:
+                    if 'translations' in sense.keys():
+                        translations = sense['translations']
+                        for translation in translations:
+                            en.append(translation['text'])
+        return en
 
 
 class DictEntry:
     def __init__(self, entry_json, status_code):
         self._json = entry_json
+        self.category = entry_json['category']
+        self.de = entry_json['de']
         if status_code == 200:
             self.found = True
-            self.category = entry_json['category']
-            self.de = entry_json['de']
             self.root = entry_json['root']
-            self.grammar_string = gen_grammar_string(entry_json['category'], entry_json['root'], entry_json['grammar'])
-            self.en_string = gen_english_string(entry_json['en'])
-
-
-def sort_grammar(gram_fe):
-    counter = {}
-    # count the occurrences of each grammatical type
-    for feature in gram_fe:
-        g_type = feature['type']
-        if g_type in counter.keys():
-            counter[g_type] += 1
+            self.grammar_string = self.gen_grammar_string(entry_json['category'], entry_json['root'], entry_json['grammar'])
+            self.en_string = self.gen_english_string(entry_json['en'])
         else:
-            counter[g_type] = 1
-    # determine the maximum
-    maximum = max(counter, key=(lambda key: counter[key]))
-    # create the empty dictionaries
-    sorted = [{} for _ in range(counter[maximum])]
+            self.found = False
+            self. grammar_string = "Unable to find {} in dictionary".format(self.de)
 
-    # create another dictionary to keep track of how many times a type has occurred while sorting.
-    occurred = {}
-    for key in counter.keys():
-        occurred[key] = 0
+    @staticmethod
+    def gen_english_string(english):
+        en_string = ''
+        for word in english:
+            en_string = en_string + ', ' + word
+        en_string = en_string[2:len(en_string)]
+        return en_string
 
-    # sort the features
-    for i in range(len(gram_fe)):
-        g_type = gram_fe[i]['type']
-        text = gram_fe[i]['text']
-        o = occurred[g_type]
-        sorted[o][g_type] = text
-        if o == counter[g_type] - 1:
-            for j in range(o, len(sorted)):
-                sorted[j][g_type] = text
-        occurred[g_type] += 1
+    @staticmethod
+    def gen_grammar_string(category, root, grammar):
+        if category == 'noun':
+            return DictEntry.gen_noun_string(grammar[0], root)
 
-    return sorted
-
-
-def gen_english_string(english):
-    en_string = ''
-    for word in english:
-        en_string = en_string + ', ' + word
-    en_string = en_string[2:len(en_string)]
-    return en_string
-
-def gen_grammar_string(category, root, grammar):
-    if category == 'noun':
-        return gen_noun_string(grammar[0], root)
+    @staticmethod
+    def gen_noun_string(grammar, root):
+        if grammar['Number'] == 'Singular':
+            return "Singular noun, {}".format(grammar['Gender'].lower())
+        else:
+            return "Plural form of: <i>{}</i>".format(root)
 
 
-def gen_noun_string(grammar, root):
-    if grammar['Number'] == 'Singular':
-        return "Singular noun, {}". format(grammar['Gender'].lower())
-    else:
-        return "Plural form of: <i>{}</i>".format(root)
 
 
-def sort_english(entries):
-    en = []
-    for entry in entries:
-        if 'senses' in entry.keys():
-            senses = entry['senses']
-            for sense in senses:
-                if 'translations' in sense.keys():
-                    translations = sense['translations']
-                    for translation in translations:
-                        en.append(translation['text'])
-    return en
 
