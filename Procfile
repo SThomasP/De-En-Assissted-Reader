8806b2dfdfbc4bf6e60200c56ef509d569dcdcb6
Steffan P <stp1g15@soton.ac.uk>
2017-11-06 14:09:22 +0000

    Setting up app to run on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn third_year_project:app
--- third_year_project.py
+++ third_year_project.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template, request
 from dictionary import Dictionary
 from reader import ReadingAssistant
-import json
+import os
 
-with open('config.json') as config_file:
-    config = json.load(config_file)
+dict_config = {'app_id': os.environ.get('APP_ID'), 'app_key': os.environ.get('APP_KEY')}
 
 app = Flask(__name__)
-dictionary = Dictionary(config['oxfordDictionaries'])
+dictionary = Dictionary(dict_config)
 
-
+'''
 @app.route('/dict/json/<word>')
 def json_lookup(word):
     entry = dictionary.lookup_json(word)
     return entry
+'''
 
 
 @app.route('/dict/html/<word>')
 def html_lookup(word):
     dict_entry = dictionary.lookup_html(word)
     return render_template("entry.html", entry=dict_entry)
+
 
 @app.route('/read')
 def read_article():
